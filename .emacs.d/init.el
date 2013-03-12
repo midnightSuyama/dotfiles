c31a7b87b69146562e569456c38d140cabeddd18
midnightSuyama <midnightSuyama@gmail.com>
2013-03-13 00:15:51 +0900

    changed init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -33,3 +33,32 @@
        (setq ns-alternate-modifier (quote super))
        (tool-bar-mode -1)
 ))
+
+;; load-path
+;;(add-to-list 'load-path "~/.emacs.d/elisp")
+
+;; auto-install
+;;(require 'auto-install)
+;;(setq auto-install-directory "~/.emacs.d/elisp/")
+;;(auto-install-update-emacswiki-package-name t)
+;;(auto-install-compatibility-setup)
+
+;; auto-complete
+;;(require 'auto-complete)
+;;(require 'auto-complete-config)
+;;(global-auto-complete-mode t)
+;;(setq ac-auto-start t)
+
+;; cc-mode
+;;(require 'cc-mode)
+;;(add-hook 'c-mode-common-hook '(lambda () (setq c-basic-offset 4)))
+
+;; php-mode
+;;(require 'php-mode)
+;;(setq php-mode-force-pear t)
+;;(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+
+;; ruby-mode
+;;(autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
+;;(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+;;(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
