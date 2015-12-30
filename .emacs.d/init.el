c2435f393daea356c410e7b1c62e4b16d58cf4e6
midnightSuyama <midnightSuyama@gmail.com>
2015-12-31 03:00:07 +0900

    Tweak

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -172,6 +172,7 @@
 ;;(require 'web-mode)
 ;;(add-to-list 'auto-mode-alist '("\\.html?$" . web-mode))
 ;;(add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
+;;(add-to-list 'auto-mode-alist '("\\.tag$" . web-mode))
 ;;(add-hook 'web-mode-hook '(lambda () (setq-default indent-tabs-mode nil) (setq web-mode-markup-indent-offset 2) (setq web-mode-css-indent-offset 2) (setq web-mode-code-indent-offset 2) (setq web-mode-style-padding 2) (setq web-mode-script-padding 2)))
 
 ;; css-mode
