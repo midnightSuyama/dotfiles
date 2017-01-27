46127f71fb629cff9f3d09094b09727da768528a
midnightSuyama <midnightSuyama@gmail.com>
2017-01-28 01:40:26 +0900

    Tweak

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,9 +23,6 @@
 ;; Tab
 (setq-default tab-width 4 indent-tabs-mode nil)
 
-;; Space
-(setq-default show-trailing-whitespace t)
-
 ;; Bell
 (setq visible-bell t)
 (setq ring-bell-function 'ignore)
@@ -65,17 +62,24 @@
 ;;(cask-initialize)
 
 ;; exec-path-from-shell
+;;(setq exec-path-from-shell-check-startup-files nil)
 ;;(when (memq window-system '(mac ns))
 ;;    (exec-path-from-shell-initialize))
 
-;; yasnippet
-;;(yas-global-mode t)
+;; multi-term
+;;(global-set-key (kbd "C-c t") 'multi-term)
+;;(add-hook 'term-mode-hook
+;;          '(lambda ()
+;;             (yas-minor-mode -1)
+;;             (define-key term-raw-map (kbd "C-n") '(lambda () (interactive) (term-send-raw-string "\C-n")))
+;;             (define-key term-raw-map (kbd "C-p") '(lambda () (interactive) (term-send-raw-string "\C-p")))
+;;))
 
-;; auto-complete
-;;(ac-config-default)
-;;(setq ac-use-menu-map t)
-;;(define-key ac-menu-map (kbd "C-n") 'ac-next)
-;;(define-key ac-menu-map (kbd "C-p") 'ac-previous)
+;; powerline
+;;(setq ns-use-srgb-colorspace nil)
+;;(set-face-attribute 'mode-line nil :foreground "#FFF" :background "#E91E63" :box nil)
+;;(set-face-attribute 'mode-line-inactive nil :foreground "#FFF" :background "#9E9E9E" :box nil)
+;;(powerline-default-theme)
 
 ;; helm
 ;;(helm-mode t)
@@ -90,9 +94,36 @@
 ;;(define-key helm-read-file-map  (kbd "TAB") 'helm-execute-persistent-action)
 ;;(setq helm-delete-minibuffer-contents-from-point t)
 
+;; helm-swoop
+;;(require 'helm-swoop)
+;;(global-set-key (kbd "M-s") 'helm-swoop)
+;;(define-key isearch-mode-map (kbd "M-s") 'helm-swoop-from-isearch)
+;;(define-key helm-swoop-map (kbd "M-s") 'helm-multi-swoop-all-from-helm-swoop)
+;;(define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
+;;(define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
+;;(define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)
+;;(define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
+
+;; helm-ls-git
+;;(define-key global-map (kbd "C-x C-g") 'helm-ls-git-ls)
+
+;; projectile
+;;(projectile-mode t)
+;;(projectile-rails-global-mode)
+;;(helm-projectile-on)
+
 ;; popwin
 ;;(require 'popwin)
 ;;(popwin-mode t)
+
+;; yasnippet
+;;(yas-global-mode t)
+
+;; auto-complete
+;;(ac-config-default)
+;;(setq ac-use-menu-map t)
+;;(define-key ac-menu-map (kbd "C-n") 'ac-next)
+;;(define-key ac-menu-map (kbd "C-p") 'ac-previous)
 
 ;; flycheck
 ;;(global-flycheck-mode)
@@ -100,6 +131,9 @@
 ;; magit
 ;;(global-set-key (kbd "C-c g") 'magit-status)
 ;;(setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
+
+;; git-gutter
+;;(global-git-gutter-mode t)
 
 ;; expand-region
 ;;(global-set-key (kbd "C-@") 'er/expand-region)
@@ -117,27 +151,42 @@
 ;;(define-key region-bindings-mode-map "m" 'mc/mark-more-like-this-extended)
 
 ;; highlight-symbol
-;;(global-set-key (kbd "M-s h") 'highlight-symbol)
-;;(global-set-key (kbd "M-s n") 'highlight-symbol-next)
-;;(global-set-key (kbd "M-s p") 'highlight-symbol-prev)
+;;(global-set-key (kbd "C-c h") 'highlight-symbol)
+
+;; highlight-indentation
+;;(add-hook 'prog-mode-hook '(lambda () (highlight-indentation-mode t)))
 
 ;; anzu
 ;;(global-anzu-mode t)
 
+;; avy
+;;(global-set-key (kbd "C-:") 'avy-goto-char-timer)
+;;(global-set-key (kbd "M-g g") 'avy-goto-line)
+
+;; smartparens
+;;(smartparens-global-mode t)
+
+;; rainbow-delimiters
+;;(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
 ;; neotree
 ;;(setq neo-keymap-style 'concise)
 ;;(setq neo-smart-open t)
-;;(global-set-key (kbd "C-c t") 'neotree-toggle)
+;;(global-set-key (kbd "C-c n") 'neotree-toggle)
 
-;; dash-at-point
-;;(global-set-key (kbd "C-c d") 'dash-at-point)
-;;(global-set-key (kbd "C-c e") 'dash-at-point-with-docset)
-
-;; osx-dictionary
-;;(global-set-key (kbd "C-c C-d") 'osx-dictionary-search-pointer)
+;; minimap
+;;(setq minimap-window-location 'right)
+;;(setq minimap-minimum-width 20)
+;;(setq minimap-automatically-delete-window nil)
+;;(global-set-key (kbd "C-c m") 'minimap-mode)
 
 ;; c-mode
-;;(add-hook 'c-mode-common-hook '(lambda () (setq c-basic-offset 4)))
+;;(add-hook 'c-mode-common-hook '(lambda () (setq c-basic-offset 4) (setq highlight-indentation-offset 4)))
+
+;; irony
+;;(add-hook 'c-mode-hook 'irony-mode)
+;;(add-hook 'c++-mode-hook 'irony-mode)
+;;(add-hook 'objc-mode-hook 'irony-mode)
 
 ;; csharp-mode
 ;;(add-hook 'csharp-mode-hook '(lambda () (omnisharp-mode) (flycheck-mode)))
@@ -159,20 +208,16 @@
 ;;(setq ruby-insert-encoding-magic-comment nil)
 
 ;; ruby-electric
-;;(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+;;(add-hook 'ruby-mode-hook 'ruby-electric-mode)
 
 ;; robe
 ;;(setq robe-mode-map nil)
 ;;(add-hook 'ruby-mode-hook 'robe-mode)
 ;;(add-hook 'robe-mode-hook 'ac-robe-setup)
 
-;; rinari
-;;(add-hook 'ruby-mode-hook 'rinari-launch)
-
-;; emmet-mode
-;;(add-hook 'sgml-mode-hook 'emmet-mode)
-;;(add-hook 'web-mode-hook 'emmet-mode)
-;;(add-hook 'css-mode-hook 'emmet-mode)
+;; jedi
+;;(add-hook 'python-mode-hook 'jedi:setup)
+;;(setq jedi:complete-on-dot t)
 
 ;; web-mode
 ;;(add-to-list 'auto-mode-alist '("\\.html?$" . web-mode))
@@ -187,17 +232,36 @@
 ;;(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
 ;;(add-hook 'scss-mode-hook '(lambda () (setq scss-compile-at-save nil)))
 
-;; js-mode
-;;(add-to-list 'auto-mode-alist '("\\.js\\.erb$" . js-mode))
-;;(add-hook 'js-mode-hook '(lambda () (setq js-indent-level 2)))
+;; emmet-mode
+;;(add-hook 'sgml-mode-hook 'emmet-mode)
+;;(add-hook 'web-mode-hook 'emmet-mode)
+;;(add-hook 'css-mode-hook 'emmet-mode)
+
+;; js2-mode
+;;(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;;(add-hook 'js2-mode-hook '(lambda () (setq js2-basic-offset 2)))
+;;(setq js2-strict-missing-semi-warning nil)
+
+;; tern
+;;(add-hook 'js2-mode-hook 'tern-mode)
+;;(eval-after-load 'tern '(progn (tern-ac-setup)))
 
 ;; yaml-mode
 ;;(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
 ;; markdown-mode
-;;(autoload 'markdown-mode "markdown-mode")
 ;;(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 ;;(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
+;; adoc-mode
+;;(add-to-list 'auto-mode-alist '("\\.adoc$" . adoc-mode))
+
+;; dash-at-point
+;;(global-set-key (kbd "C-c d") 'dash-at-point)
+;;(global-set-key (kbd "C-c e") 'dash-at-point-with-docset)
+
+;; osx-dictionary
+;;(global-set-key (kbd "C-c C-d") 'osx-dictionary-search-pointer)
 
 ;; Marked 2
 ;;(defun markdown-preview-file ()
@@ -207,7 +271,4 @@
 ;;   (format "open -a 'Marked 2.app' %s"
 ;;       (shell-quote-argument (buffer-file-name))))
 ;;)
-;;(global-set-key (kbd "C-c m") 'markdown-preview-file)
-
-;; adoc-mode
-;;(add-to-list 'auto-mode-alist '("\\.adoc$" . adoc-mode))
+;;(global-set-key (kbd "C-c C-m") 'markdown-preview-file)
